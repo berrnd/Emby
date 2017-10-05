016a46b92461a810b5cd17e6a31a8691d459eac6
Bernd Bestel <bernd@berrnd.de>
2017-10-05 09:45:44 +0200

    Added missing file

---
+++ Emby.Server.Implementations/StringArrayExtensions.cs
@@ -0,0 +1,21 @@
+﻿//myproduction-change-start
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emby.Server.Implementations
+{
+	public static class StringArrayExtensions
+	{
+		public static string[] Add(this string[] array, string stringToAppend)
+		{
+			int n = array.Length;
+			Array.Resize(ref array, n + 1);
+			array[n] = stringToAppend;
+			return array;
+		}
+	}
+}
+//myproduction-change-end
